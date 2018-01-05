06f9f66c2f48b8c7336ca638c05fc4dc9dd1e6a7
johanesteves <jsesteves91@gmail.com>
2018-01-05 16:53:27 -0500

    Setup rack-cors

--- board-game-api/db/seeds.rb
+++ board-game-api/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gloomhaven = BoardGame.create(name: "Gloomhaven", rank: 1, rating: 9.0, img_url: "", release_year: 2017, weight: 3.33)
+
+pandemic_legacy_S1 = BoardGame.create(name: "Pandemic Legacy: Season 1", rank: 2, rating: 8.7, img_url: "", release_year: 2015, weight: 2.80)
