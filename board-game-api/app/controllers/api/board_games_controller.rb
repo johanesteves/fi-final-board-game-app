61bc8246f7972e06a0e173e2c3fe066d1f2496ea
johanesteves <jsesteves91@gmail.com>
2018-01-05 17:20:22 -0500

    Added update, show, destroy, create actions in controller

--- board-game-api/app/controllers/api/board_games_controller.rb
+++ board-game-api/app/controllers/api/board_games_controller.rb
@@ -1,8 +1,43 @@
 class Api::BoardGamesController < ApplicationController
-
   def index
-    @board_games = BoardGame.all
-    render json: @board_games
+    render json: BoardGame.all
   end
 
+  def create
+    board_game = BoardGame.new(board_game_params)
+    if board_game.save
+      render json: board_game
+    else
+      render json: {message: board_game.errors}, status: 400
+    end
+  end
+
+  def show
+    board_game = BoardGame.find(params[:id])
+    render json: board_game
+  end
+
+  def update
+    board_game = BoardGame.find(params[:id])
+    if board_game.update(board_game_params)
+      render json: board_game
+    else
+      render json: { message: board_game.errors }, status: 400
+    end
+  end
+
+  def destroy
+    board_game = BoardGame.find(params[:id])
+    if board_game.destroy
+      render status: 204
+    else
+      render json: { message: "Cannot delete board game." }, status: 400
+    end
+  end
+
+  private
+
+  def board_game_params
+    params.require(:board_game).permit(:name, :rank, :rating, :img_url, :release_year, :weight)
+  end
 end
