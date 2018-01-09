024443396b3fad472b95ebe520665d4a80b0031f
johanesteves <jsesteves91@gmail.com>
2018-01-09 17:40:46 -0500

    removed state to instead use event value in onChange function

--- board-game-client/src/containers/BoardGameSearchContainer.js
+++ board-game-client/src/containers/BoardGameSearchContainer.js
@@ -2,33 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { searchBoardGame } from '../actions/boardgames';
 import BoardGameList from "../components/BoardGameList";
+import BoardGameSearchForm from "../components/BoardGameSearchForm";
 
 
 class BoardGameSearchContainer extends Component{
-    constructor(){
-        super();
-        this.state = {
-            searchText: ''
-        }
-    }
-
     handleOnChange = (event) => {
-        this.setState({
-            searchText: event.target.value
-        });
-
         this.props.searchBoardGame(event.target.value);
-
     };
 
     render(){
         return(
             <div>
-                <form>
-                    <label htmlFor="searchText" className="col-md-4 control-label">Search: </label>
-                    <input name="searchText" type="text" onChange={this.handleOnChange} />
-                </form>
-                <div><h3>Results:</h3></div>
+                <BoardGameSearchForm handleOnChange={this.handleOnChange}/>
                 <BoardGameList boardgames={this.props.searchResults}/>
             </div>
         )
