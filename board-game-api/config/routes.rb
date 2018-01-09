988340117c69b326f3cdab54b6e9cbe93f5881ff
johanesteves <jsesteves91@gmail.com>
2018-01-09 10:46:17 -0500

    Added search route

--- board-game-api/config/routes.rb
+++ board-game-api/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/api/board_games/search/:query', to: 'api/board_games#search'
+
   namespace :api do
     resources :board_games, except: [:new, :edit]
   end
+
+
 end
+BoardGame.where('name LIKE ?', "%#{params[:query]}%")
