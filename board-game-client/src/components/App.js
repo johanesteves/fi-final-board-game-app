67e6855924346b2ff3a0492ac64cf8d83aeb8c25
johanesteves <jsesteves91@gmail.com>
2018-01-09 17:37:53 -0500

    Added Router, Routes, NavBar components

--- board-game-client/src/components/App.js
+++ board-game-client/src/components/App.js
@@ -3,16 +3,22 @@ import './App.css'
 import BoardGames from '../containers/BoardGames'
 import BoardGameContainer from '../containers/BoardGameContainer';
 import BoardGameSearchContainer from "../containers/BoardGameSearchContainer";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import BoardGameForm from "../containers/BoardGameForm";
 
 class App extends Component{
     render(){
         return (
-            <div className="App">
-                <BoardGames/>
-                <br></br>
-                <BoardGameContainer/>
-                <BoardGameSearchContainer/>
-            </div>
+            <Router>
+                <div className="App">
+                    <NavBar />
+                    <Route exact path="/" component={ BoardGames } />
+                    <Route exact path="/boardgames" component={ BoardGameContainer } />
+                    <Route exact path="/search" component={ BoardGameSearchContainer } />
+                    <Route exact path="/new" component={ BoardGameForm } />
+                </div>
+            </Router>
         )
     }
 }
