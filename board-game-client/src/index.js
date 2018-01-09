2fd131d7f27bc3b046d104251eca60ef3cea2a93
johanesteves <jsesteves91@gmail.com>
2018-01-08 20:54:43 -0500

    Added Provider tag to index

--- board-game-client/src/index.js
+++ board-game-client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 
-console.log(store);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
