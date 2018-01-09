5ed6f558568dd5000fd2d24f73011ccdfd24d741
johanesteves <jsesteves91@gmail.com>
2018-01-08 19:29:12 -0500

    Added componentDidMount fetch request to return boardgames in App

--- board-game-client/src/containers/App.js
+++ board-game-client/src/containers/App.js
@@ -4,7 +4,22 @@ import BoardGames from './BoardGames'
 import BoardGameForm from "./BoardGameForm";
 
 class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            boardgames: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3001/api/board_games')
+            .then(response => response.json())
+            .then(boardgames => this.setState({boardgames}))
+    }
+
     render(){
+        console.log(this.state.boardgames);
         return (
             <div className="App">
                 <BoardGames/>
