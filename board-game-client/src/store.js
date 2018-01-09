b55345906a26675276ab04230fbeeb12040d7e09
johanesteves <jsesteves91@gmail.com>
2018-01-08 21:57:09 -0500

    Addd boardGameFormData to track form data in state

--- board-game-client/src/store.js
+++ board-game-client/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import boardgames from './reducers/boardgames'
+import boardGameFormData from './reducers/boardGameFormData'
 
 
 const reducers = combineReducers({
-    boardgames
+    boardgames,
+    boardGameFormData
 });
 
 const middleware = [thunk];
