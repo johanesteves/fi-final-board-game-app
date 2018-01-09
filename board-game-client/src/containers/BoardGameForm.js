15e1328a03b23ea7ab9a57d60e1346a360e3398f
johanesteves <jsesteves91@gmail.com>
2018-01-08 21:55:16 -0500

    Added dispatch to connect and update handleSubmit to use dispatch prop

--- board-game-client/src/containers/BoardGameForm.js
+++ board-game-client/src/containers/BoardGameForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createBoardGame } from '../actions/boardgames'
 
 
 class BoardGameForm extends Component{
@@ -22,35 +24,32 @@ class BoardGameForm extends Component{
 
     handleOnSubmit = event => {
         event.preventDefault();
-        this.setState({
-            name: '',
-            rating: 0,
-            rank: 0,
-            img_url: '',
-            release_year: 0,
-            weight: 0.00
-        })
+        const boardgame = Object.assign({}, this.state);
+        this.props.createBoardGame(boardgame);
+        this.setState({name: '', rating: 0, rank: 0, img_url: '', release_year: 0, weight: 0.00})
     };
 
 
     render(){
+        const { name, rating, release_year, img_url, weight } = this.props.boardGameFormData;
+
         return(
             <div className="container">
                 <form onSubmit={this.handleOnSubmit}>
                     <label htmlFor="name" className="col-md-4 control-label">Name</label>
-                    <input name="name" type="text" onChange={this.handleOnChange}/>
+                    <input name="name" type="text"  value={name} onChange={this.handleOnChange}/>
                     <br></br>
                     <label htmlFor="rating" className="col-md-4 control-label">Rating</label>
-                    <input name="rating" type="number" step="0.5" max="10" min="0" onChange={this.handleOnChange}/>
+                    <input name="rating" type="number" value={rating} step="0.5" max="10" min="0" onChange={this.handleOnChange}/>
                     <br></br>
                     <label htmlFor="img_url" className="col-md-4 control-label">Image URL</label>
-                    <input name="img_url" type="text" onChange={this.handleOnChange}/>
+                    <input name="img_url" type="text" value={img_url} onChange={this.handleOnChange}/>
                     <br></br>
                     <label htmlFor="release_year" className="col-md-4 control-label">Release Year</label>
-                    <input name="release_year" type="number" min="1990" max="2018" step="1" onChange={this.handleOnChange}/>
+                    <input name="release_year" type="number" value={release_year} min="1990" max="2018" step="1" onChange={this.handleOnChange}/>
                     <br></br>
                     <label htmlFor="weight" className="col-md-4 control-label">Weight</label>
-                    <input name="weight" type="number" step="0.5" max="5" min="0" onChange={this.handleOnChange}/>
+                    <input name="weight" type="number" value={weight} step="0.5" max="5" min="0" onChange={this.handleOnChange}/>
                     <br></br>
                     <input  type="submit"/>
                 </form>
@@ -59,4 +58,10 @@ class BoardGameForm extends Component{
     }
 }
 
-export default BoardGameForm
+const mapStateToProps = (state) => {
+    return ({
+        boardGameFormData: state.boardGameFormData
+    })
+};
+
+export default connect(mapStateToProps, { createBoardGame })(BoardGameForm);
