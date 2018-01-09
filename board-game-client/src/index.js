590a6a4709afaf44db7a62f074e14f4420e7e7d7
johanesteves <jsesteves91@gmail.com>
2018-01-08 20:02:36 -0500

    Added store.js and added store to index.js

--- board-game-client/src/index.js
+++ board-game-client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store'
+
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- board-game-client/src/store.js
+++ board-game-client/src/store.js
@@ -1,2 +1,24 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+
+const boardGameReducer = (state=[], action) => {
+    switch(action.type){
+        case 'ADD_BOARD_GAME':
+            return action.boardgames;
+        default:
+            return state;
+    }
+};
+
+const reducers = combineReducers({
+    boardgames: boardGameReducer
+});
+
+const middleware = [thunk];
+
+
+export default createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware),
+);
