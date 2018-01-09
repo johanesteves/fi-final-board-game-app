2a84a69c75954d620858b253c0a588d65101b702
johanesteves <jsesteves91@gmail.com>
2018-01-08 21:53:14 -0500

    Added createBoardGame action to use fetch API and POST

--- board-game-client/src/actions/boardgames.js
+++ board-game-client/src/actions/boardgames.js
@@ -1,8 +1,14 @@
 const setBoardGames = boardgames => {
-    debugger;
     return {
         type: 'GET_BOARD_GAMES',
         boardgames
+    }
+};
+
+const addBoardGame = boardgame => {
+    return {
+        type: 'ADD_BOARD_GAME',
+        boardgame
     }
 };
 
@@ -12,6 +18,18 @@ export const getBoardGames = () => {
             .then(response => response.json())
             .then(boardgames => dispatch(setBoardGames(boardgames)))
             .catch(error => console.log(error))
+    }
+};
+
+export const createBoardGame = boardgame => {
+    return dispatch => {
+        return fetch('http://localhost:3001/api/board_games', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({board_game: boardgame})
+        })
+            .then(response => response.json())
+            .then(boardgame => dispatch(addBoardGame(boardgame)))
     }
 };
 
