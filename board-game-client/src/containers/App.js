5db5f424d194c7ce0b25c3a6005e53ddf5ef6ae7
johanesteves <jsesteves91@gmail.com>
2018-01-08 20:53:48 -0500

    Added GameList component to App. Updated Gamelist to use BoardGameCard component

--- board-game-client/src/containers/App.js
+++ board-game-client/src/containers/App.js
@@ -2,29 +2,17 @@ import React, {Component} from 'react';
 import './App.css'
 import BoardGames from './BoardGames'
 import BoardGameForm from "./BoardGameForm";
+import BoardGameList from './BoardGameList';
 
 class App extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            boardgames: []
-        }
-    }
-
-    componentDidMount(){
-        fetch('http://localhost:3001/api/board_games')
-            .then(response => response.json())
-            .then(boardgames => this.setState({boardgames}))
-    }
-
     render(){
-        console.log(this.state.boardgames);
         return (
             <div className="App">
                 <BoardGames/>
                 <br></br>
                 <BoardGameForm/>
+                <br></br>
+                <BoardGameList/>
             </div>
         )
     }
--- board-game-client/src/containers/BoardGameList.js
+++ board-game-client/src/containers/BoardGameList.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import BoardGameCard from '../components/BoardGameCard';
+import { getBoardGames } from '../actions/boardgames';
 // import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class BoardGameList extends Component{
+
+    componentDidMount() {
+        this.props.getBoardGames()
+    }
+
     render(){
         return(
             <div>
-                <BoardGameCard/>
+                {this.props.boardgames.map((boardgame) => {
+                    return (<BoardGameCard key={boardgame.id} boardgame={boardgame}/>)
+                })}
             </div>
         )
     }
 }
 
-export default BoardGameList
+const mapStateToProps = (state) => {
+    return({
+        boardgames: state.boardgames
+    })
+};
+
+export default connect(mapStateToProps, { getBoardGames })(BoardGameList);
