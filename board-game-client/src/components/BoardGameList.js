0960ad3a20230746ca114fad193fa3f64f9eeaf7
johanesteves <jsesteves91@gmail.com>
2018-01-09 21:13:31 -0500

    Added div using bootstrap classnames

--- board-game-client/src/components/BoardGameList.js
+++ board-game-client/src/components/BoardGameList.js
@@ -3,10 +3,14 @@ import BoardGameCard from '../components/BoardGameCard';
 
 const BoardGameList = (props) => {
     return (
-        <div>
+        <div className="album text-muted">
+            <div className="container">
+                <div className="row">
             {props.boardgames.map((boardgame) => {
                 return (<BoardGameCard key={boardgame.id} boardgame={boardgame}/>)
             })}
+                </div>
+            </div>
         </div>
     )
 };
