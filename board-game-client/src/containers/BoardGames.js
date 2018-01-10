a12a6739c5c669766e9a96eb9e667e003c2e94cf
johanesteves <jsesteves91@gmail.com>
2018-01-09 21:16:11 -0500

    Updated Homepage  with bootstrap classnames

--- board-game-client/src/containers/BoardGames.js
+++ board-game-client/src/containers/BoardGames.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BoardGames = ()=>{
     return (
         <div className="homePage">
-            Here are the list of all current board games
+            <section className="jumbotron text-center">
+                <div className="container">
+                    <h1 className="jumbotron-heading ">Board Game Collection</h1>
+                    <p className="lead text-muted">
+                        Create a collection of your board games. Rate them as your play them!
+                    </p>
+                    <p><Link to="/boardgames" className="btn btn-primary">View my collection</Link></p>
+                </div>
+            </section>
         </div>
     )
 };
