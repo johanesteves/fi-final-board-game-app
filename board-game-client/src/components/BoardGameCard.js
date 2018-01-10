a9029079798a4174dd32e0950aa43ff1b5110e1c
johanesteves <jsesteves91@gmail.com>
2018-01-09 21:14:16 -0500

    Added div using bootstrap classnames. Updated data displayed in each card. Added onError function to catch for img_url errors

--- board-game-client/src/components/BoardGameCard.js
+++ board-game-client/src/components/BoardGameCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+function imgError(event) {
+    event.target.src = "http://vollrath.com/ClientCss/images/VollrathImages/No_Image_Available.jpg";
+}
+
 const BoardGameCard = (props) => {
     return (
-        <div>
-            <li>Name: {props.boardgame.name}</li>
-            <li>Weight: {props.boardgame.weight}</li>
-            <li>Rank: {props.boardgame.rank}</li>
-            <br></br>
+        <div className="card">
+            <img className="img-card" src={props.boardgame.img_url} onError={imgError} alt={props.boardgame.name}/>
+            <p className="card-text">
+                <strong>{props.boardgame.name}</strong> <br />
+                <small>
+                    <em>Release Year: </em> {props.boardgame.release_year} <br />
+                    <em>Weight: </em> {props.boardgame.weight} <br />
+                    <em>Rating: </em> {props.boardgame.rating}
+                </small>
+            </p>
         </div>
     )
 };
