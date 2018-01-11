594991b97804a55100c1d03c95f742793465f008
johanesteves <jsesteves91@gmail.com>
2018-01-10 19:29:46 -0500

    Created Show container for show route

---
+++ board-game-client/src/containers/BoardGameShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BoardGameShow = props => {
+
+    return (
+        <div>
+            <h3>BoardGame Show Component!</h3>
+        </div>
+    );
+};
+
+export default BoardGameShow;
