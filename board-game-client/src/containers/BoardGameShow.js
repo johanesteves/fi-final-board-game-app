1291f938d2f7cc0b17f82ad63d91a2e21cc1c2ea
johanesteves <jsesteves91@gmail.com>
2018-01-10 20:02:58 -0500

    Updated BoardGameShow containter to use params props and render game based on param id

--- board-game-client/src/containers/BoardGameShow.js
+++ board-game-client/src/containers/BoardGameShow.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const BoardGameShow = props => {
-
     return (
-        <div>
-            <h3>BoardGame Show Component!</h3>
+        <div className="container-fluid bg-1 text-center">
+            <h3 className="margin">{props.boardGame.name}</h3>
+            <img src={props.boardGame.img_url} className="img-responsive rounded-circle margin" alt={props.boardGame.name} width="350" height="350" />
+            <p>
+                <small>
+                    <em>Release Year: </em> {props.boardGame.release_year} <br />
+                    <em>Weight: </em> {props.boardGame.weight} <br />
+                    <em>Rating: </em> {props.boardGame.rating}
+                </small>
+            </p>
         </div>
     );
 };
 
-export default BoardGameShow;
+
+const mapStateToProps = (state, ownProps) => {
+    const boardGame = state.boardgames.find(boardgame => boardgame.id === parseInt(ownProps.match.params.boardGameId));
+
+    if (boardGame){
+        return { boardGame}
+    }else{
+        return {boardGame: {}}
+    }
+};
+
+export default connect(mapStateToProps)(BoardGameShow);
