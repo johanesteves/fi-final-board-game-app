a54da0c9d89ccc6bd54e8a1e6af49de99d5d3f0e
johanesteves <jsesteves91@gmail.com>
2018-01-10 21:32:40 -0500

    Updated function to use radix

--- board-game-client/src/containers/BoardGameShow.js
+++ board-game-client/src/containers/BoardGameShow.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getBoardGames } from '../actions/boardgames';
 
-
-class  BoardGameShow extends React.Component{
-
-    componentDidMount() {
-        this.props.getBoardGames()
-    }
-
-    render(){
+const  BoardGameShow = props => {
     return (
         <div className="container-fluid bg-1 text-center">
-            <h3 className="margin">{this.props.boardGame.name}</h3>
-            <img src={this.props.boardGame.img_url} className="img-responsive rounded-circle margin" alt={this.props.boardGame.name} width="350" height="350" />
+            <h3 className="margin">{props.boardGame.name}</h3>
+            <img src={props.boardGame.img_url} className="img-responsive rounded-circle margin" alt={props.boardGame.name} width="350" height="350" />
             <p>
                 <small>
-                    <em>Release Year: </em> {this.props.boardGame.release_year} <br />
-                    <em>Weight: </em> {this.props.boardGame.weight} <br />
-                    <em>Rating: </em> {this.props.boardGame.rating}
+                    <em>Release Year: </em> {props.boardGame.release_year} <br />
+                    <em>Weight: </em> {props.boardGame.weight} <br />
+                    <em>Rating: </em> {props.boardGame.rating}
                 </small>
             </p>
         </div>
-    )}
-}
+    );
+};
 
 
 const mapStateToProps = (state, ownProps) => {
@@ -36,4 +28,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export default connect(mapStateToProps, {getBoardGames})(BoardGameShow);
+export default connect(mapStateToProps)(BoardGameShow);
