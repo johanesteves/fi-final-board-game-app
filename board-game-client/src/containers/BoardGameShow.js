5eeaab4bfb52f1dcb9b2524e01e99fdc91619bfa
johanesteves <jsesteves91@gmail.com>
2018-01-10 21:30:09 -0500

    Updated function to use radix

--- board-game-client/src/containers/BoardGameShow.js
+++ board-game-client/src/containers/BoardGameShow.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getBoardGames } from '../actions/boardgames';
 
-const BoardGameShow = props => {
+
+class  BoardGameShow extends React.Component{
+
+    componentDidMount() {
+        this.props.getBoardGames()
+    }
+
+    render(){
     return (
         <div className="container-fluid bg-1 text-center">
-            <h3 className="margin">{props.boardGame.name}</h3>
-            <img src={props.boardGame.img_url} className="img-responsive rounded-circle margin" alt={props.boardGame.name} width="350" height="350" />
+            <h3 className="margin">{this.props.boardGame.name}</h3>
+            <img src={this.props.boardGame.img_url} className="img-responsive rounded-circle margin" alt={this.props.boardGame.name} width="350" height="350" />
             <p>
                 <small>
-                    <em>Release Year: </em> {props.boardGame.release_year} <br />
-                    <em>Weight: </em> {props.boardGame.weight} <br />
-                    <em>Rating: </em> {props.boardGame.rating}
+                    <em>Release Year: </em> {this.props.boardGame.release_year} <br />
+                    <em>Weight: </em> {this.props.boardGame.weight} <br />
+                    <em>Rating: </em> {this.props.boardGame.rating}
                 </small>
             </p>
         </div>
-    );
-};
+    )}
+}
 
 
 const mapStateToProps = (state, ownProps) => {
-    const boardGame = state.boardgames.find(boardgame => boardgame.id === parseInt(ownProps.match.params.boardGameId));
+    const boardGame = state.boardgames.find(boardgame => boardgame.id === parseInt(ownProps.match.params.boardGameId, 10));
 
     if (boardGame){
         return { boardGame}
@@ -28,4 +36,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export default connect(mapStateToProps)(BoardGameShow);
+export default connect(mapStateToProps, {getBoardGames})(BoardGameShow);
